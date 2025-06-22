5870806d21f01cb2ab77cfa750c77fab4f139a06
Acquerello11 <baitouy2547@gmail.com>
2025-06-22 17:37:22 +0700

    web

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
